ba328cc12fac9a746e35535658b1548943b6fed1
muluken360 <muluken@360ground.com>
2026-01-24 14:37:16 +0300

    corrceted configiration and deployments

--- src/auth/better-auth.config.ts
+++ src/auth/better-auth.config.ts
@@ -9,10 +9,10 @@ export const auth = betterAuth({
     provider: 'postgresql',
   }),
   // FIX 1: Use Environment Variable for baseURL so it doesn't default to localhost on Render
-  baseURL: process.env.BETTER_AUTH_URL 
+  baseURL: process.env.BETTER_AUTH_URL ,
 
   // FIX 2: Add secret (Required for production)
-  secret: process.env.BETTER_AUTH_SECRET,
+  secret: process.env.BETTER_AUTH_SECRET as string,
   trustedOrigins: [
     'http://localhost:3000',
     'http://localhost:3001',
